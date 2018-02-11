c012a7b31d494b49f067d0b186d702877bccddfd
kratosc2 <30858446+kratosc2@users.noreply.github.com>
2018-02-11 06:00:15 +0000

    Add files via upload

---
+++ cgi-bin/1.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cgi, cgitb, os, sys
+from bs4 import BeautifulSoup
+import string
+import random
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
+
+
+path  = '/var/www/html/upload/'
+files = os.listdir(path)
+
+ab = 0
+
+
+
+print 'Content-Type: text/html; charset=UTF-8'
+print
+print '''
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="http://127.0.0.1/style.css">
+
+<body>
+  <title>Upload File</title>
+  <audio onended=nextrack() hidden id="aplayer" controls>
+
+</audio>
+
+  <div class = "box">
+  <div class =aline> </div>
+    <div id =nowplay class = "nowplay"> True Detective - Intro / Opening Song - Theme (The Handsome Family - Far From Any Road)  Now Playing... </div>
+  <button id = "ply" class="plybtn" onclick="playb()"> <img src=http://94.54.178.57/play.png></button>
+  <div onclick=getpos(event) onmousedown=funcstart() onmousemove=func(event) onmouseup=funcstop() id="duratline" class= "durationline">
+ <div class=selectedduration></div> 
+  <div onmousemove=selecthov(event)  onmouseover=selecthov(event) onmouseout="outhover()" onclick=outhover() id = "point" class= "pointer">
+
+    <div id = "time" class="timec">0:00</div>
+
+  </div>
+</div>
+
+  <button id = "mutebtn" class="plybtn1" onclick="muteb()"> <img src=http://94.54.178.57/loud.png></button>
+
+    <div id = "vol" onclick ="volumechng(event)" class="volume"> <div id ="volpoint" class = "volumepointer"> </div></div>
+
+
+<div style=margin-top:40px class=aline></div>
+<span id=tooltipid class =tooltip></span>
+</div>
+<p style=text-align:center;color:white;background:rgb(0,100,0);> List 
+'''
+
+for x in files:
+    ab = ab + 1 
+    xa = 'http://127.0.0.1/upload/'+x
+    xa = xa.replace(' ','%20')
+    x = x.replace('.mp3','')
+    x = str(ab) + ' - ' + x
+    print '<div name="'+str(ab)+'" id="'+xa+'" href="'+x+'" onclick=play(c="'+xa+'") class=list><a onclick =play(c="'+xa+'") href=#><b tid ="'+x+'" id=namet >'+x+'</b></a></div>'
+print'''
+
+</body>
+<script src = "http://127.0.0.1/player.js"></script>
+
+
+</html>
+'''
---
+++ html/player.js
@@ -0,0 +1,239 @@
+var player = document.getElementById('aplayer');
+player.addEventListener("timeupdate", updateplayer, false);
+var nowplay = document.getElementById('nowplay');
+var lock = false;
+var tn = 0;
+
+function play(d){
+	if (tn > 0){
+	var before = document.getElementsByName(tn)[0].id;
+        document.getElementById(before).style.borderRadius = "5px";
+        document.getElementById(before).style.backgroundColor = "green";
+
+	
+	}
+	var nametrack = document.getElementById(d).getAttribute('href');
+	var na = document.getElementById(d).getAttribute('name');
+	var player = document.getElementById('aplayer');
+	nowplay.innerHTML = nametrack + (' Now Playing...');
+        player.src = c;
+	tn = na;
+        player.load();
+        player.play();
+	document.getElementById(d).style.borderRadius = "5px";
+	document.getElementById(d).style.backgroundColor = "gold";
+
+}
+
+function funcstart(){
+	lock = true;
+
+}
+
+function funcstop(){
+	lock = false;
+
+}
+
+
+
+function func(event){
+    if (lock == true){
+	var player = document.getElementById('aplayer');
+	var duration = player.duration;	
+    	var divw = document.getElementById('duratline').offsetWidth;
+    	var div2 = document.getElementById('point');
+    	var pos = event.clientX - 8;
+	var sperc = parseInt(pos / (divw/100));
+	var sdurat = parseInt(duration* (sperc/100));
+	player.currentTime = parseInt(sdurat) - 15;
+
+	}
+}
+
+
+
+function playb(){
+  var player = document.getElementById("aplayer");
+  if (player.paused){
+    var btn = document.getElementById("ply");
+    btn.innerHTML = "<img src=http://94.54.178.57/pause.png>"
+    player.play();
+
+  }
+  else{
+    var btn = document.getElementById("ply");
+    btn.innerHTML = "<img src=http://94.54.178.57/play.png>";
+    player.pause();
+  }
+
+}
+
+
+var duratline = document.getElementById('duratline');
+duratline.addEventListener("click",getpos);
+
+function getpos(event){
+  var he = document.getElementById("point");
+  var x = event.clientX - 79;
+  var player = document.getElementById("aplayer");
+  var durat = player.duration;
+  var y = document.getElementById('duratline').offsetWidth
+  var z = x / (y/100);
+  player.currentTime = durat * (z/100);
+
+  player.play();
+
+}
+
+
+vol.addEventListener("click",volumechng);
+function volumechng(event){
+  var al = document.getElementById('nowplay');
+  var volum = document.getElementById("vol");
+  var post = volum.offsetWidth;
+  var clickp = event.clientX;
+  var screenw = screen.width;
+  var posit = screenw - clickp;
+  var pointvol = document.getElementById("volpoint");
+  var player = document.getElementById("aplayer");
+  var diff = screenw - post;
+  var varr = screenw - diff;
+  var abc  = clickp/100;
+  var varrcli = clickp - screenw;
+  var theend = parseInt(varrcli/(varr/100));
+  var th = theend + 105;
+  if (th > 99) {
+    var a = 1;
+  }
+  else {
+    a = 0;
+  }
+  if (th < 10){
+    th = "0" + th;
+  }
+  else {
+    th = th;
+  }
+
+  pointvol.style.width = th + "%";
+  var the = a + "." + (th);
+  player.volume = abc;
+  al.innerHTML = the;
+}
+function muteb(){
+  var player = document.getElementById("aplayer");
+  
+if (player.volume == 1){
+  var pointvol = document.getElementById("volpoint");
+  pointvol.style.width = 0 + "%";
+  player.volume = 0;
+  var btnna = document.getElementById("mutebtn");
+  btnna.innerHTML = "<img src=http://94.54.178.57/mute.png>";
+}
+  else{
+    var pointvol = document.getElementById("volpoint");
+    pointvol.style.width = 100 +"%";
+    player.volume = 1;
+    var btnna = document.getElementById("mutebtn");
+    btnna.innerHTML = "<img src=http://94.54.178.57/loud.png>";
+  }
+}
+
+
+
+var lockhover = false;
+
+function oth(){
+
+
+}
+
+function selecthov(event){	
+	lockhover = true;
+	var offset = document.getElementById('duratline').offsetWidth;
+	var player = document.getElementById('aplayer');
+	var duration = player.duration;
+	var durationline = document.getElementById('duratline');
+	var pointerhov = document.getElementById('point');
+	var timer = document.getElementById('time');
+	var x = event.pageX - 79;
+	var y = event.pageY - 79;
+	if (x < 0){
+		x = 0
+	}
+	var ap = durationline.offsetWidth;
+	var sperc = x/(offset/100) ;
+	var sstime = duration * (sperc/100);
+	var curr = 100 * (player.currentTime / duration);
+	var secs = parseInt(sstime % 60);
+	var mins = parseInt(sstime / 60 );
+	if (secs < 10){
+		secs = '0' + secs;
+	}
+	document.getElementById('tooltipid').style.top = y + 30 + 'px';
+	document.getElementById('tooltipid').style.left = x + 60 + 'px';
+	document.getElementById('tooltipid').innerHTML = mins +':' +secs + ' '+ tn;
+	document.getElementById('tooltipid').style.opacity = "1";
+	pointerhov.style.background = "linear-gradient(to right,seagreen "+curr+"%,gold "+(curr + 1)+"%,gold "+sperc+"%,  gold "+sperc+"%,white "+sperc+ "%)";
+	pointerhov.style.transition = "1s";
+
+
+}
+
+function outhover(){
+	lockhover = false;
+	var pointerhov = document.getElementById('point');
+	pointerhov.style.background = "white";
+        document.getElementById('tooltipid').style.opacity = "0";
+
+}
+
+function updateplayer(){
+        var player = document.getElementById("aplayer");
+        var point = document.getElementById("point");
+        var duration = player.duration;
+        var percent = 100 * (player.currentTime / duration);
+        var time = document.getElementById("time")
+        var secs = parseInt(player.currentTime % 60);
+        var secstomin = parseInt(player.currentTime);
+  if (secs < 10){
+    secs = "0" + secs
+  }
+
+  var mins = parseInt(secstomin / 60);
+
+  time.innerHTML = mins + ":" + secs ;
+        if (lockhover == false){
+  point.style.background = "linear-gradient(to right, seagreen "+percent+"%,white "+percent+ "%)";
+
+        }
+}
+
+
+function nextrack(){
+	var before = document.getElementsByName(tn)[0].id;
+	document.getElementById(before).style.borderRadius = "5px";
+        document.getElementById(before).style.backgroundColor = "green";
+
+
+	var nowplayid = document.getElementById('nowplay');
+	var nextdata = parseInt(parseInt(tn)+ 1);
+	var player = document.getElementById('aplayer');
+	var next = document.getElementsByName(nextdata)[0].id;
+
+	nowplayid.innerHTML = document.getElementById(next).getAttribute('href')+' Now Playing ...' ;
+	player.src = next;
+	player.load();
+	player.play();
+	tn = parseInt(tn) + 1;
+        document.getElementById(next).style.borderRadius = "5px";
+        document.getElementById(next).style.backgroundColor = "gold";
+
+
+}
+
+
+
+
+
